f3d2c05b960df393784075128d981013e3dda64a
kiatt210 <kiatt210@gmail.com>
2024-05-16 13:51:13 +0200

    Dockerfile

--- docker_build.sh
+++ docker_build.sh
@@ -3,4 +3,4 @@ JAR_FILE=$1
 VERSION=$2
 echo "jar: $JAR_FILE"
 echo "Version: $VERSION"
-docker buildx build --builder rpi-builder --platform linux/arm/v7 --build-arg="JAR_FILE=target/${JAR_FILE}" --tag 192.168.0.20:5000/auto-downloader:${VERSION} --push .
+docker buildx build -f docker/Dockerfile --builder rpi-builder --platform linux/arm/v7 --build-arg="JAR_FILE=target/${JAR_FILE}" --tag 192.168.0.20:5000/auto-downloader:${VERSION} --push .
